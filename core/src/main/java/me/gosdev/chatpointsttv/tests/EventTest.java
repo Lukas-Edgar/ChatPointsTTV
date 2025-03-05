0d10e3bc6603fc11f9fd99abd304128fcbe256a3
lukas-edgar <trashymctrash7+github@gmail.com>
2025-03-05 20:52:19 +0100

    refactor test package

--- core/src/main/java/me/gosdev/chatpointsttv/tests/EventTest.java
+++ core/src/main/java/me/gosdev/chatpointsttv/tests/EventTest.java
@@ -18,10 +18,22 @@ import me.gosdev.chatpointsttv.ChatPointsTTV;
 import me.gosdev.chatpointsttv.utils.TwitchUtils;
 
 public class EventTest {
-    
-    
+
+    private EventTest() {}
+
+    public static final String CHATTER_USER_ID = "{\"chatter_user_id\":\"";
+    public static final String CHATTER_USER_LOGIN = "\",\"chatter_user_login\":\"";
+    public static final String CHATTER_USER_NAME = "\",\"chatter_user_name\":\"";
+    public static final String BROADCASTER_USER_ID = "\",\"broadcaster_user_id\":\"";
+    public static final String BROADCASTER_USER_LOGIN = "\",\"broadcaster_user_login\":\"";
+    public static final String BROADCASTER_USER_NAME = "\",\"broadcaster_user_name\":\"";
+    public static final String USER_NAME = "\",\"user_name\":\"";
+    public static final String USER_LOGIN = "\",\"user_login\":\"";
+    public static final String USER_ID = "{\"user_id\":\"";
+    public static final String NOTICE_TYPE_SUB = "\",\"notice_type\": \"sub\"";
+
     @SuppressWarnings("deprecation")
-    public static RewardRedeemedEvent ChannelPointsRedemptionEvent(String channel, String chatter, String title, Optional<String> userInput) { //TODO: Move away from PubSub
+    public static RewardRedeemedEvent channelPointsRedemptionEvent(String channel, String chatter, String title, Optional<String> userInput) { //TODO: Move away from PubSub
         ChannelPointsRedemption redemption = new ChannelPointsRedemption();
         ChannelPointsReward reward = new ChannelPointsReward();
         ChannelPointsUser redeemer = new ChannelPointsUser();
@@ -31,100 +43,99 @@ public class EventTest {
 
         redemption.setChannelId(TwitchUtils.getUserId(channel));
         redemption.setUser(redeemer);
-        if (userInput.isPresent()) {
-            redemption.setUserInput(userInput.get());
-        }
+        userInput.ifPresent(redemption::setUserInput);
 
         redemption.setReward(reward);
 
         return new RewardRedeemedEvent(Instant.now(), redemption);
     }
 
-    public static ChannelFollowEvent FollowEvent(String channel, String chatter) {
-        ChannelFollowEvent event = jsonToObject(
-            "{\"user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"user_login\":\"" + chatter.toLowerCase() +
-            "\",\"user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
+    public static ChannelFollowEvent followEvent(String channel, String chatter) {
+
+        return jsonToObject(
+            USER_ID + TwitchUtils.getUserId(chatter) +
+                    USER_LOGIN + chatter.toLowerCase() +
+                    USER_NAME + chatter +
+                    BROADCASTER_USER_ID + TwitchUtils.getUserId(channel) +
+                    BROADCASTER_USER_LOGIN + channel.toLowerCase() +
+                    BROADCASTER_USER_NAME + channel +
             "\",\"followed_at\":\"" + Instant.now().toString() +"\"}",
         ChannelFollowEvent.class);
-
-        return event;
     }
 
-    public static ChannelChatMessageEvent CheerEvent(String channel, String chatter, int amount) {
-        ChannelChatMessageEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"cheer\": {\"bits\": " + amount +"}}", 
+    public static ChannelChatMessageEvent cheerEvent(String channel, String chatter, int amount) {
+
+        return jsonToObject(
+            CHATTER_USER_ID + TwitchUtils.getUserId(chatter) +
+                    CHATTER_USER_LOGIN + chatter.toLowerCase() +
+                    CHATTER_USER_NAME + chatter +
+                    BROADCASTER_USER_ID + TwitchUtils.getUserId(channel) +
+                    BROADCASTER_USER_LOGIN + channel.toLowerCase() +
+                    BROADCASTER_USER_NAME + channel +
+            "\",\"cheer\": {\"bits\": " + amount +"}}",
         ChannelChatMessageEvent.class);
-        
-        return event;
     }
 
-    public static ChannelChatNotificationEvent SubEvent(String channel, String chatter, SubscriptionPlan plan, int months) {
+    public static ChannelChatNotificationEvent subEvent(String channel, String chatter, SubscriptionPlan plan, int months) {
         ChannelChatNotificationEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
+            CHATTER_USER_ID + TwitchUtils.getUserId(chatter) +
+                    CHATTER_USER_LOGIN + chatter.toLowerCase() +
+                    CHATTER_USER_NAME + chatter +
+                    BROADCASTER_USER_ID + TwitchUtils.getUserId(channel) +
+                    BROADCASTER_USER_LOGIN + channel.toLowerCase() +
+                    BROADCASTER_USER_NAME + channel +
             "\",\"notice_type\": \"sub" +
             "\",\"sub\":{\"sub_tier\":\"" + (plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "1000" : plan.toString()) + 
-            "\",\"is_prime\":" + (plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "true" : "false") +
+            isPrime(plan) +
             ",\"duration_months\":" + months + "}}",
             ChannelChatNotificationEvent.class);
 
-        ChatPointsTTV.getPlugin().log.info(plan.toString());
+        ChatPointsTTV.getInstance().log.info(plan.toString());
 
         return event;
             }
 
-    public static ChannelChatNotificationEvent ResubEvent(String channel, String chatter, SubscriptionPlan plan, int months) {
-        ChannelChatNotificationEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"notice_type\": \"sub\"" +
-            "\",resub\":{\"sub_tier\":\"" + plan.name() + 
-            "\",\"is_prime\":" + (plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "true" : "false") +
-            "\",is_gift\": \"false\"" +
-            "\",duration_months\":" + months + 
-            "\",cumulative_months\":" + months + 
-            "\",streak_months\":" + months + "}}",
-            ChannelChatNotificationEvent.class);
+    public static ChannelChatNotificationEvent resubEvent(String channel, String chatter, SubscriptionPlan plan, int months) {
 
-            return event;
+        return jsonToObject(
+                CHATTER_USER_ID + TwitchUtils.getUserId(chatter) +
+                        CHATTER_USER_LOGIN + chatter.toLowerCase() +
+                        CHATTER_USER_NAME + chatter +
+                        BROADCASTER_USER_ID + TwitchUtils.getUserId(channel) +
+                        BROADCASTER_USER_LOGIN + channel.toLowerCase() +
+                        BROADCASTER_USER_NAME + channel +
+                        NOTICE_TYPE_SUB +
+                        "\",resub\":{\"sub_tier\":\"" + plan.name() +
+                        isPrime(plan) +
+                        "\",is_gift\": \"false\"" +
+                        "\",duration_months\":" + months +
+                        "\",cumulative_months\":" + months +
+                        "\",streak_months\":" + months + "}}",
+            ChannelChatNotificationEvent.class);
             }
 
-    public static ChannelChatNotificationEvent SubGiftEvent(String channel, String chatter, SubscriptionPlan plan, int amount) {
-        ChannelChatNotificationEvent event = jsonToObject(
-            "{\"chatter_user_id\":\"" + TwitchUtils.getUserId(chatter) +
-            "\",\"chatter_user_login\":\"" + chatter.toLowerCase() +
-            "\",\"chatter_user_name\":\"" + chatter +
-            "\",\"broadcaster_user_id\":\"" + TwitchUtils.getUserId(channel) +
-            "\",\"broadcaster_user_login\":\"" + channel.toLowerCase() +
-            "\",\"broadcaster_user_name\":\"" + channel +
-            "\",\"notice_type\": \"community_sub_gift" +
-            "\",\"community_sub_gift\":{\"sub_tier\":\"" + plan + 
-            "\",\"total\":" + amount + "}}",
-            ChannelChatNotificationEvent.class);
-
-            return event;
+    private static String isPrime(SubscriptionPlan plan) {
+        return "\",\"is_prime\":" + (plan.equals(SubscriptionPlan.TWITCH_PRIME) ? "true" : "false");
     }
 
-    public static ChannelRaidEvent RaidReward(String channel, String raider, int viewers) {
-        ChannelRaidEvent event = jsonToObject(
+    public static ChannelChatNotificationEvent subGiftEvent(String channel, String chatter, SubscriptionPlan plan, int amount) {
+
+        return jsonToObject(
+            CHATTER_USER_ID + TwitchUtils.getUserId(chatter) +
+                    CHATTER_USER_LOGIN + chatter.toLowerCase() +
+                    CHATTER_USER_NAME + chatter +
+                    BROADCASTER_USER_ID + TwitchUtils.getUserId(channel) +
+                    BROADCASTER_USER_LOGIN + channel.toLowerCase() +
+                    BROADCASTER_USER_NAME + channel +
+            "\",\"notice_type\": \"community_sub_gift" +
+            "\",\"community_sub_gift\":{\"sub_tier\":\"" + plan +
+            "\",\"total\":" + amount + "}}",
+            ChannelChatNotificationEvent.class);
+    }
+
+    public static ChannelRaidEvent raidReward(String channel, String raider, int viewers) {
+
+        return jsonToObject(
             "{\"from_broadcaster_user_id\":\"" + TwitchUtils.getUserId(raider) +
             "\",\"from_broadcaster_user_login\":\"" + raider.toLowerCase() +
             "\",\"from_broadcaster_user_name\":\"" + raider +
@@ -133,7 +144,5 @@ public class EventTest {
             "\",\"to_broadcaster_user_name\":\"" + channel +
             "\",\"viewers\": \"" + viewers + "\"}",
             ChannelRaidEvent.class);
-
-        return event;
     }
 }
--- core/src/main/java/me/gosdev/chatpointsttv/tests/TestCommand.java
+++ core/src/main/java/me/gosdev/chatpointsttv/tests/TestCommand.java
@@ -2,13 +2,19 @@ package me.gosdev.chatpointsttv.tests;
 
 import com.github.philippheuer.events4j.core.EventManager;
 import com.github.twitch4j.common.enums.SubscriptionPlan;
-
 import me.gosdev.chatpointsttv.ChatPointsTTV;
 import me.gosdev.chatpointsttv.utils.MalformedArgumentsException;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.function.Predicate.not;
 
 public class TestCommand {
-    private static final EventManager eventManager = ChatPointsTTV.getPlugin().getTwitch().getClient().getEventManager();
+    private static final EventManager eventManager = ChatPointsTTV.getInstance().getTwitch().getClient().getEventManager();
 
     public static void test(CommandSender sender, String[] cmdInput) {
         if (cmdInput.length < 2) {
@@ -16,188 +22,164 @@ public class TestCommand {
             return;
         }
 
-        ArrayList<String> args =  new ArrayList<>();
+        ArrayList<String> args = getArguments(cmdInput);
+
+        try {
+            startEvent(args);
+        } catch (MalformedArgumentsException e) {
+            ChatPointsTTV.getUtils().sendMessage(sender, e.getMessage());
+        }
+
+
+        ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.GREEN + "Test event sent!");
+    }
+
+    private static ArrayList<String> getArguments(String[] cmdInput) { //TODO: refactor
+        ArrayList<String> args = new ArrayList<>();
         for (int i = 0; i < cmdInput.length; i++) {
             String arg = cmdInput[i];
             // Check if the argument starts with a quote and does not end with an escaped quote
             if (arg.startsWith("\"") && !arg.endsWith("\\\"")) {
-            StringBuilder sb = new StringBuilder(arg.substring(1));
-            // Continue appending arguments until the closing quote is found
-            while (i + 1 < cmdInput.length && !(cmdInput[i + 1].endsWith("\"") && !cmdInput[i + 1].endsWith("\\\""))) {
-                sb.append(" ").append(cmdInput[++i]);
-            }
-            // Append the last part of the quoted argument
-            if (i + 1 < cmdInput.length) {
-                sb.append(" ").append(cmdInput[++i], 0, cmdInput[i].length() - 1);
-            }
-            // Add the complete quoted argument to the args list
-            args.add(sb.toString().replace("\\\"", "\""));
+                StringBuilder sb = new StringBuilder(arg.substring(1));
+                // Continue appending arguments until the closing quote is found
+                while (i + 1 < cmdInput.length && !(cmdInput[i + 1].endsWith("\"") && !cmdInput[i + 1].endsWith("\\\""))) {
+                    sb.append(" ").append(cmdInput[++i]);
+                }
+                // Append the last part of the quoted argument
+                if (i + 1 < cmdInput.length) {
+                    sb.append(" ").append(cmdInput[++i], 0, cmdInput[i].length() - 1);
+                }
+                // Add the complete quoted argument to the args list
+                args.add(sb.toString().replace("\\\"", "\""));
             } else {
-            // Add the argument to the args list, replacing escaped quotes
-            args.add(arg.replace("\\\"", "\""));
+                // Add the argument to the args list, replacing escaped quotes
+                args.add(arg.replace("\\\"", "\""));
             }
         }
+        return args;
+    }
 
+    private static void startEvent(ArrayList<String> args) {
         switch (args.get(1).toLowerCase()) {
             case "channelpoints":
-                if (args.size() < 5) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Usage: /twitch test channelpoints <redeemer> <channel> <reward> [userInput]");
-                    return;
-                }
-
-                String pointsChatter = args.get(2);
-                String pointsChannel = args.get(3);
-                String pointsReward = args.get(4);
-                String userInput;
-
-                if (args.size() <= 5) {
-                    userInput = null;
-                } else {
-                    for (int i = 6; i < args.size(); i++) {
-                        args.set(5, args.get(5) + " " + args.get(i)); // All arguments after index 4 are considered user input
-                    }
-                    userInput = args.get(5);
-                }
-                try {
-                    eventManager.publish(EventTest.ChannelPointsRedemptionEvent(pointsChannel, pointsChatter, pointsReward, userInput != null ? Optional.of(userInput) : Optional.empty()));
-                } catch (NullPointerException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + e.getMessage());
-                    return;
-                }
+                startChannelPointsEvent(args);
                 break;
             case "follow":
-                if (args.size() < 4) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Usage: /twitch test follow <user> <channel>");
-                    return;
-                }
-
-                String followUser = args.get(2);
-                String followChannel = args.get(3);
-
-                try {
-                    eventManager.publish(EventTest.FollowEvent(followChannel, followUser));
-                } catch (NullPointerException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + e.getMessage());
-                    return;
-                }
+                startFollowEvent(args);
                 break;
-
             case "cheer":
-                if (args.size() < 5) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Usage: /twitch test cheer <user> <channel> <amount>");
-                    return;
-                }
-
-                String cheerUser = args.get(2);
-                String cheerChannel = args.get(3);
-                int cheerAmount;
-
-                try {
-                    cheerAmount = Integer.parseInt(args.get(4));
-                } catch (NumberFormatException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Invalid cheer amount: " + args.get(4));
-                    return;
-                }
-
-                try {
-                    eventManager.publish(EventTest.CheerEvent(cheerChannel, cheerUser, cheerAmount));
-                } catch (NullPointerException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + e.getMessage());
-                    return;
-                }
+                startCheerEvent(args);
                 break;
-
             case "sub":
-                if (args.size() < 6) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Usage: /twitch test sub <user> <channel> <plan> <months>");
-                    return;
-                }
-
-                String subUser = args.get(2);
-                String subChannel = args.get(3);
-                SubscriptionPlan subTier;
-                int subMonths;
-
-                try {
-                    subTier = SubscriptionPlan.valueOf(args.get(4).toUpperCase());
-                    subMonths = Integer.parseInt(args.get(5));
-                } catch (NumberFormatException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Invalid amount of months: " + args.get(5));
-                    return;
-                } catch (IllegalArgumentException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Invalid subscription tier: " + args.get(4));
-                    return;
-                }
-                
-
-                try {
-                    eventManager.publish(EventTest.SubEvent(subChannel, subUser, subTier, subMonths));
-                } catch (NullPointerException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + e.getMessage());
-                    return;
-                }
+                startSubEvent(args);
                 break;
-
             case "subgift":
-                if (args.size() < 6) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Usage: /twitch test subgift <user> <channel> <tier> <amount>");
-                    return;
-                }
-
-                String giftChatter = args.get(2);
-                String giftChannel = args.get(3);
-                SubscriptionPlan giftTier;
-                int giftAmount;
-
-                try {
-                    giftAmount = Integer.parseInt(args.get(5));
-                    giftTier = SubscriptionPlan.valueOf(args.get(4).toUpperCase());
-                } catch (NumberFormatException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Invalid gifted subs amount: " + args.get(5));
-                    return;
-                } catch (IllegalArgumentException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Invalid subscription tier: " + args.get(4));
-                    return;
-                }
-                
-                try {
-                    eventManager.publish(EventTest.SubGiftEvent(giftChannel, giftChatter, giftTier, giftAmount));
-                } catch (NullPointerException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + e.getMessage());
-                    return;
-                }
+                startSubGiftEvent(args);
                 break;
-
             case "raid":
-                if (args.size() < 5) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Usage: /twitch test raid <raider> <channel> <viewer count>");
-                    return;
-                }
-
-                String raidUser = args.get(2);
-                String raidChannel = args.get(3);
-                int raidViewers;
-
-                try {
-                    raidViewers = Integer.parseInt(args.get(4));
-                } catch (NumberFormatException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Invalid viewer amount: " + args.get(4));
-                    return;
-                }
-                
-                try {
-                    eventManager.publish(EventTest.RaidReward(raidChannel, raidUser, raidViewers));
-                } catch (NullPointerException e) {
-                    ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + e.getMessage());
-                    return;
-                }
+                startRaidEvent(args);
                 break;
-
             default:
-                ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.RED + "Unknown test type: " + args.get(1));
-                return;
+                throw new MalformedArgumentsException(ChatColor.RED + "Unknown test type: " + args.get(1));
+        }
+    }
+
+    private static void startRaidEvent(ArrayList<String> args) {
+        if (args.size() < 5) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Usage: /twitch test raid <raider> <channel> <viewer count>");
         }
 
-        ChatPointsTTV.getUtils().sendMessage(sender, ChatColor.GREEN + "Test event sent!");
+        String raidUser = args.get(2);
+        String raidChannel = args.get(3);
+        int raidViewers;
+
+        try {
+            raidViewers = Integer.parseInt(args.get(4));
+        } catch (NumberFormatException e) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Invalid viewer amount: " + args.get(4));
+        }
+
+        eventManager.publish(EventTest.raidReward(raidChannel, raidUser, raidViewers));
+    }
+
+    private static void startSubGiftEvent(ArrayList<String> args) {
+        if (args.size() < 6) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Usage: /twitch test subgift <user> <channel> <tier> <amount>");
+        }
+
+        String giftChatter = args.get(2);
+        String giftChannel = args.get(3);
+        try {
+            int giftAmount = Integer.parseInt(args.get(5));
+            SubscriptionPlan giftTier = SubscriptionPlan.valueOf(args.get(4).toUpperCase());
+            eventManager.publish(EventTest.subGiftEvent(giftChannel, giftChatter, giftTier, giftAmount));
+        } catch (NumberFormatException e) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Invalid gifted subs amount: " + args.get(5));
+        } catch (IllegalArgumentException e) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Invalid subscription tier: " + args.get(4));
+        }
+    }
+
+    private static void startSubEvent(ArrayList<String> args) {
+        if (args.size() < 6) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Usage: /twitch test sub <user> <channel> <plan> <months>");
+        }
+        String subUser = args.get(2);
+        String subChannel = args.get(3);
+
+        try {
+            SubscriptionPlan subTier = SubscriptionPlan.valueOf(args.get(4).toUpperCase());
+            int subMonths = Integer.parseInt(args.get(5));
+            eventManager.publish(EventTest.subEvent(subChannel, subUser, subTier, subMonths));
+        } catch (NumberFormatException e) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Invalid amount of months: " + args.get(5));
+        } catch (IllegalArgumentException e) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Invalid subscription tier: " + args.get(4));
+        }
+
+
+    }
+
+    private static void startCheerEvent(ArrayList<String> args) {
+        if (args.size() < 5) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Usage: /twitch test cheer <user> <channel> <amount>");
+        }
+
+        String cheerUser = args.get(2);
+        String cheerChannel = args.get(3);
+
+        try {
+            int cheerAmount = Integer.parseInt(args.get(4));
+            eventManager.publish(EventTest.cheerEvent(cheerChannel, cheerUser, cheerAmount));
+        } catch (NumberFormatException e) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Invalid cheer amount: " + args.get(4));
+        }
+
+    }
+
+    private static void startFollowEvent(ArrayList<String> args) {
+        if (args.size() < 4) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Usage: /twitch test follow <user> <channel>");
+        }
+
+        String followUser = args.get(2);
+        String followChannel = args.get(3);
+
+        eventManager.publish(EventTest.followEvent(followChannel, followUser));
+    }
+
+    private static void startChannelPointsEvent(ArrayList<String> args) {
+        if (args.size() < 5) {
+            throw new MalformedArgumentsException(ChatColor.RED + "Usage: /twitch test channelpoints <redeemer> <channel> <reward> [userInput]");
+        }
+
+        String pointsChatter = args.get(2);
+        String pointsChannel = args.get(3);
+        String pointsReward = args.get(4);
+
+        String collect = args.stream().skip(5).collect(Collectors.joining(" "));
+        Optional<String> userInput = Optional.of(collect).filter(not(String::isEmpty));
+
+        eventManager.publish(EventTest.channelPointsRedemptionEvent(pointsChannel, pointsChatter, pointsReward, userInput));
     }
 }
