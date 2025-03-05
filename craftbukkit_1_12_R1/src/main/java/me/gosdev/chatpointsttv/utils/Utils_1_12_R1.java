9ee5d70e84f2be2b2851ad61ec085672f5498a30
lukas-edgar <trashymctrash7+github@gmail.com>
2025-03-05 20:42:54 +0100

    use new enum

--- craftbukkit_1_12_R1/src/main/java/me/gosdev/chatpointsttv/utils/Utils_1_12_R1.java
+++ craftbukkit_1_12_R1/src/main/java/me/gosdev/chatpointsttv/utils/Utils_1_12_R1.java
@@ -7,36 +7,35 @@ import org.bukkit.entity.Player;
 import me.gosdev.chatpointsttv.ChatPointsTTV;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 
-public class Utils_1_9_R1 implements Utils {    @SuppressWarnings("deprecation")
+public class Utils_1_12_R1 implements Utils {
     @Override
     public void displayTitle(Player p, String title, String action, String sub, Boolean bold, ChatColor titleColor, ChatColor subColor) {
-        if (bold) {
-            p.sendTitle(titleColor + title, action + " " + subColor + ChatColor.BOLD + sub);
-        } else {
-            p.sendTitle(titleColor + title, action + " " + subColor + sub);
-        }
+        ChatColor format = bold ? ChatColor.BOLD : ChatColor.RESET;
+        p.sendTitle(titleColor + title, action + subColor + " " + format + sub, 10, 70, 20);
     }
     
     @Override
     public void sendMessage(CommandSender p, BaseComponent[] message) {
-        Bukkit.getPlayer(p.getName()).spigot().sendMessage(message);
+        p.spigot().sendMessage(message);
     }
 
     @Override
     public void sendMessage(CommandSender p, BaseComponent message) {
-        Bukkit.getPlayer(p.getName()).spigot().sendMessage(message);
+        p.spigot().sendMessage(message);
     }
     @Override
     public void sendMessage(CommandSender p, String message) {
-        p.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD +"[ChatPointsTTV] " + ChatColor.RESET + message);
+        BaseComponent component = new ComponentBuilder(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD +"[ChatPointsTTV] " + ChatColor.RESET + message).create()[0];
+        p.spigot().sendMessage(component);
     }
-
     @Override
     public void sendLogToPlayers(String msg) {
+        BaseComponent component = new ComponentBuilder(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD +"[ChatPointsTTV] " + ChatColor.RESET + msg).create()[0];
         for (Player p : Bukkit.getOnlinePlayers()) {
-            if (p.hasPermission(ChatPointsTTV.permissions.MANAGE.permission_id)) {
-                p.sendRawMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "[ChatPointsTTV] " + ChatColor.RESET + msg);
+            if (p.hasPermission(Permissions.MANAGE.permissionId)) {
+                p.spigot().sendMessage(component);
             }
         }
     }
--- craftbukkit_1_9_R1/src/main/java/me/gosdev/chatpointsttv/utils/Utils_1_9_R1.java
+++ craftbukkit_1_9_R1/src/main/java/me/gosdev/chatpointsttv/utils/Utils_1_9_R1.java
@@ -7,35 +7,36 @@ import org.bukkit.entity.Player;
 import me.gosdev.chatpointsttv.ChatPointsTTV;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
 
-public class Utils_1_12_R1 implements Utils {
+public class Utils_1_9_R1 implements Utils {    @SuppressWarnings("deprecation")
     @Override
     public void displayTitle(Player p, String title, String action, String sub, Boolean bold, ChatColor titleColor, ChatColor subColor) {
-        ChatColor format = bold ? ChatColor.BOLD : ChatColor.RESET;
-        p.sendTitle(titleColor + title, action + subColor + " " + format + sub, 10, 70, 20);
+        if (bold) {
+            p.sendTitle(titleColor + title, action + " " + subColor + ChatColor.BOLD + sub);
+        } else {
+            p.sendTitle(titleColor + title, action + " " + subColor + sub);
+        }
     }
     
     @Override
     public void sendMessage(CommandSender p, BaseComponent[] message) {
-        p.spigot().sendMessage(message);
+        Bukkit.getPlayer(p.getName()).spigot().sendMessage(message);
     }
 
     @Override
     public void sendMessage(CommandSender p, BaseComponent message) {
-        p.spigot().sendMessage(message);
+        Bukkit.getPlayer(p.getName()).spigot().sendMessage(message);
     }
     @Override
     public void sendMessage(CommandSender p, String message) {
-        BaseComponent component = new ComponentBuilder(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD +"[ChatPointsTTV] " + ChatColor.RESET + message).create()[0];
-        p.spigot().sendMessage(component);
+        p.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD +"[ChatPointsTTV] " + ChatColor.RESET + message);
     }
+
     @Override
     public void sendLogToPlayers(String msg) {
-        BaseComponent component = new ComponentBuilder(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD +"[ChatPointsTTV] " + ChatColor.RESET + msg).create()[0];
         for (Player p : Bukkit.getOnlinePlayers()) {
-            if (p.hasPermission(ChatPointsTTV.permissions.MANAGE.permission_id)) {
-                p.spigot().sendMessage(component);
+            if (p.hasPermission(Permissions.MANAGE.permissionId)) {
+                p.sendRawMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "[ChatPointsTTV] " + ChatColor.RESET + msg);
             }
         }
     }
